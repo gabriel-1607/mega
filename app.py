dd74cf9913c1b37f43445239dd5ef06b911365e6
gabriel-1607 <gabrielambrosini02@gmail.com>
2025-03-15 09:37:14 -0500

    Added two new routes

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/", methods=["POST", "GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/", methods=["POST", "GET"])
+def register():
+    return render_template("register.html")
